1ced1e17ec8963d7ee6713154432657ccb0a6cb1
AndresMurillo24 <150602160+AndresMurillo24@users.noreply.github.com>
2026-02-15 23:01:48 -0500

    Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Act1;
 
 import java.util.Scanner;
@@ -13,10 +9,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Seleccione el tipo de menú:");
-        System.out.println("1. Menú Saludable");
-        System.out.println("2. Menú Vegetariano");
-        System.out.println("3. Menú Gourmet");
-        
+        System.out.println("1. Saludable");
+        System.out.println("2. Vegetariano");
+        System.out.println("3. Gourmet");
 
         int opcion = sc.nextInt();
 
@@ -24,15 +19,14 @@ public class Main {
 
         switch (opcion) {
             case 1:
-    factory = new MenuSaludableFactory();
-    break;
-case 2:
-    factory = new MenuVegetarianoFactory();
-    break;
-case 3:
-    factory = new MenuGourmetFactory();
-    break;
-
+                factory = new MenuSaludableFactory();
+                break;
+            case 2:
+                factory = new MenuVegetarianoFactory();
+                break;
+            case 3:
+                factory = new MenuGourmetFactory();
+                break;
             default:
                 System.out.println("Opción no válida");
                 return;
@@ -42,15 +36,23 @@ case 3:
         consumidor.generarMenu(factory);
 
         System.out.println("\n¿Qué desea consumir?");
-        System.out.println("1. Entrada");
-        System.out.println("2. Plato principal");
-        System.out.println("3. Bebida");
-        System.out.println("4. Postre");
-        System.out.println("5. Todo");
+        System.out.println("1. Solo Entrada");
+        System.out.println("2. Solo Plato Principal");
+        System.out.println("3. Solo Bebida");
+        System.out.println("4. Solo Postre");
+        System.out.println("5. Entrada + Plato");
+        System.out.println("6. Entrada + Postre");
+        System.out.println("7. Plato + Bebida");
+        System.out.println("8. Bebida + Postre");
+        System.out.println("9. Entrada + Plato + Bebida");
+        System.out.println("10. Entrada + Plato + Postre");
+        System.out.println("11. Plato + Bebida + Postre");
+        System.out.println("12. Todo el menú");
 
         int consumo = sc.nextInt();
 
         switch (consumo) {
+
             case 1:
                 consumidor.consumirEntrada();
                 break;
@@ -66,6 +68,37 @@ case 3:
             case 5:
                 consumidor.consumirEntrada();
                 consumidor.consumirPlato();
+                break;
+            case 6:
+                consumidor.consumirEntrada();
+                consumidor.consumirPostre();
+                break;
+            case 7:
+                consumidor.consumirPlato();
+                consumidor.consumirBebida();
+                break;
+            case 8:
+                consumidor.consumirBebida();
+                consumidor.consumirPostre();
+                break;
+            case 9:
+                consumidor.consumirEntrada();
+                consumidor.consumirPlato();
+                consumidor.consumirBebida();
+                break;
+            case 10:
+                consumidor.consumirEntrada();
+                consumidor.consumirPlato();
+                consumidor.consumirPostre();
+                break;
+            case 11:
+                consumidor.consumirPlato();
+                consumidor.consumirBebida();
+                consumidor.consumirPostre();
+                break;
+            case 12:
+                consumidor.consumirEntrada();
+                consumidor.consumirPlato();
                 consumidor.consumirBebida();
                 consumidor.consumirPostre();
                 break;
@@ -76,3 +109,4 @@ case 3:
         sc.close();
     }
 }
+
